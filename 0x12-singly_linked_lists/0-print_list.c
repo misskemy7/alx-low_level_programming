03ae42597b27409c81d5b6bd409a034d83b8471a
Ekemini Barnaby <barnabyekemini@yahoo.com>
2022-04-21 14:41:51 -0700

    hot fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
  *
- * Return: the number of nodes
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
